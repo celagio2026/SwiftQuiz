0b796f67f127b9c8320897ef7d0e3ce35ec412f2
celagio2026 <117282010+celagio2026@users.noreply.github.com>
2023-02-13 02:00:45 +0800

    Add files via upload

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
-const question = document.getElementById('question');
+
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const loader = document.getElementById('loader');
 const game = document.getElementById('game');
